470d7d4173a9e7afb4f776158ffbb7de4d4435bd
sahil <deepsahil.online@gmail.com>
2026-04-19 23:45:21 +0530

    fix: dont show preloader on normal navigation click

--- src/components/LayoutWrapper.js
+++ src/components/LayoutWrapper.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Preloader from "./Preloader2";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
@@ -10,6 +10,7 @@ import PageTransition from "@/app/PageTransition";
 export default function LayoutWrapper({ children }) {
   const pathname = usePathname();
   const isHomePage = pathname === "/";
+  const [preloaderFinished, setPreloaderFinished] = useState(false);
 
   // hide navbar/footer on auth pages
   const hideLayout = pathname.startsWith("/policies") || pathname.startsWith("/maintenance") || pathname.startsWith("/verify") || pathname.startsWith("/admin") || pathname === null;
@@ -24,7 +25,9 @@ export default function LayoutWrapper({ children }) {
 
   return (
     <>
-      {isHomePage && <Preloader />}
+      {isHomePage && !preloaderFinished && (
+        <Preloader onFinish={() => setPreloaderFinished(true)} />
+      )}
       {!hideLayout && <Navbar />}
       {/* <PageTransition> */}
       {children}
